df6dab58d6799e417522da696ce475f2400ff0ab
CMEONE <contact@kabirwearsato.ga>
2021-10-06 18:48:21 -0700

    Add hex.convertFromString, hex.convertToString

--- helpful.js
+++ helpful.js
@@ -31,5 +31,35 @@
 		return duplicated;
 	}
 
+	helpful.hex = {};
+
+	/* Modified from https://github.com/TogaTech/tEnvoy  */
+	helpful.hex.convertFromString = (string) => {
+		if(string == null) {
+			return "";
+		}
+		let hex = "";
+		for(let i = 0; i < string.length; i++) {
+			let c = string.charCodeAt(i).toString(16);
+			while(c.length < 2) {
+				c = "0" + c;
+			}
+			hex += c;
+		}
+		return hex;
+	}
+
+	/* Modified from https://github.com/TogaTech/tEnvoy  */
+	helpful.hex.convertToString = (hex) => {
+		if(hex == null) {
+			return "";
+		}
+		let string = "";
+		for(let i = 0; i < hex.length; i += 2) {
+			string += String.fromCharCode(parseInt(hex.substring(i, i + 2), 16));
+		}
+		return string;
+	}
+
 	return helpful;
 }));
