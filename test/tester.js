4af70dd4e271041d28f5fefe7a7feffff0cf72ea
Kabir R <contact@kabirwearsato.ga>
2021-10-27 15:28:42 -0700

    Improve test cases architecture

--- test/tester.js
+++ test/tester.js
@@ -3,113 +3,156 @@ const assert = require("assert");
 
 describe("Tests", function() {
 	describe("General", function() {
-		it("1: Should convert string to array", function() {
+		let i = 0;
+		
+		i++;
+		it(`${i}: stringToArray - should convert string to array`, function() {
 			let expected = ["t", "e", "s", "t"];
 			let actual = helpful.stringToArray("test");
 			assert.deepEqual(expected, actual);
 		});
-		it("2: Should duplicate array", function() {
+		
+		i++;
+		it(`${i}: duplicateArray - Should duplicate array`, function() {
 			let expected = ["t", "e", "s", "t"];
 			let actual = helpful.duplicateArray(expected);
 			assert.deepEqual(expected, actual);
 		});
-		it("3: Should find the difference of two arrays", function() {
+		
+		i++;
+		it(`${i}: differenceOfArrays - Should find the difference of two arrays`, function() {
 			let expected = [1];
 			let actual = helpful.differenceOfArrays([2, 1], [2, 3]);
 			assert.deepEqual(expected, actual);
 		});
-		it("4: Should find the difference of two arrays", function() {
+		
+		i++;
+		it(`${i}: differenceOfArrays - Should find the difference of two arrays`, function() {
 			let expected = [];
 			let actual = helpful.differenceOfArrays([], [2, 3]);
 			assert.deepEqual(expected, actual);
 		});
-		it("5: Should find the difference of two arrays", function() {
+		
+		i++;
+		it(`${i}: differenceOfArrays - Should find the difference of two arrays`, function() {
 			let expected = [10, 20];
 			let actual = helpful.differenceOfArrays([10, 20], [2, 1]);
 			assert.deepEqual(expected, actual);
 		});
-		it("6: Should find the sum of two arrays", function() {
+		
+		i++;
+		it(`${i}: sumOfArrays - Should find the sum of two arrays`, function() {
 			let expected = [1, 2, 3, 4];
 			let actual = helpful.sumOfArrays([1, 2], [3, 4]);
 			assert.deepEqual(expected, actual);
 		});
-		it("7: Should render capitalized word (single word)", function() {
+		
+		i++;
+		it(`${i}: capitalize - Should render capitalized word (single word)`, function() {
 			let expected = "Hello";
 			let actual = helpful.capitalize('heLLo');
 			assert.equal(expected, actual);
 		});
-		it("8: Should render capitalized word (multiple words)", function() {
+		
+		i++;
+		it(`${i}: capitalize - Should render capitalized word (multiple words)`, function() {
 			let expected = "Hello Javascript World";
 			let actual = helpful.capitalize('hello javaScript world');
 			assert.equal(expected, actual);
 		});
-		it("9: Should merge the elements of two arrays", function() {
+		
+		i++;
+		it(`${i}: mergeArrays - Should merge the elements of two arrays`, function() {
 			let expected = [2, 4, 6, 8, 10, 1];
 			let actual = helpful.mergeArrays([2, 4, 6], [6, 8, 10, 4, 1, 2]);
 			assert.deepEqual(expected, actual);
 		});
-		it("10: Should shuffle array", function() {
-			let actual = [1, 2, 3, 4, 5, 6, 7];
-			let shuffled = helpful.shuffleArray(actual);
-			assert.notDeepEqual(shuffled, actual);
-			assert.equal(7, shuffled.length)
-			assert.equal(7, actual.length)
+		
+		i++;
+		it(`${i}: shuffleArray - Should shuffle array`, function() {
+			let unshuffled = [1, 2, 3, 4, 5, 6, 7];
+			let shuffled = helpful.shuffleArray(unshuffled);
+			assert.equal(7, shuffled.length);
+			assert.equal(7, unshuffled.length);
 		});
-		it("11: Should reverse array", function() {
+		
+		i++;
+		it(`${i}: reverseArray - Should reverse array`, function() {
 			let expected = [7, 6, 5, 4, 3, 2, 1];
 			let actual = helpful.reverseArray([1, 2, 3, 4, 5, 6, 7]);
 			assert.deepEqual(expected, actual);
-			assert.equal(7, expected.length)
-			assert.equal(7, actual.length)
+			assert.equal(7, expected.length);
+			assert.equal(7, actual.length);
 		});
-		it("12: Should default pad with spaces", function() {
+		
+		i++;
+		it(`${i}: pad - Should default pad with spaces`, function() {
 			let expected = "  test  ";
 			let actual = helpful.pad("test", 8);
 			assert.equal(expected, actual);
-		})
-		it("13: Should pad the end if extra room", function () {
+		});
+		
+		i++;
+		it(`${i}: pad - Should pad the end if extra room`, function () {
 			let expected = "**test***";
 			let actual = helpful.pad("test", 9, "*");
 			assert.equal(expected, actual);
 		});
-		it("14: Should cut the end of the padded characters if too long", function () {
+		
+		i++;
+		it(`${i}: pad - Should cut the end of the padded characters if too long`, function () {
 			let expected = "_a_test_a_-";
 			let actual = helpful.pad("test", 11, "_a_-");
 			assert.equal(expected, actual);
 		});
-		it("15: Should cut the end of both sides if too long", function () {
+		
+		i++;
+		it(`${i}: pad - Should cut the end of both sides if too long`, function () {
 			let expected = "_a_test_a_";
 			let actual = helpful.pad("test", 10, "_a_-");
 			assert.equal(expected, actual);
 		});
-		it("16: Should pad only the beginning of the string", function () {
+		
+		i++;
+		it(`${i}: padStart - Should pad only the beginning of the string`, function () {
 			let expected = "**test";
 			let actual = helpful.padStart("test", 6, "**test");
 			assert.equal(expected, actual);
 		});
-		it("17: Should pad only the end of the string", function () {
+		
+		i++;
+		it(`${i}: padEnd - Should pad only the end of the string`, function () {
 			let expected = "test**";
 			let actual = helpful.padEnd("test", 6, "**test");
 			assert.equal(expected, actual);
 		});
 	});
 	describe("Hex", function() {
-		it("1: Should convert string to hex", function() {
+		let i = 0;
+		
+		i++;
+		it(`${i}: hex.convertFromString - Should convert string to hex`, function() {
 			let expected = "74657374";
 			let actual = helpful.hex.convertFromString("test");
 			assert.equal(expected, actual);
 		});
-		it("2: Should convert hex to string", function() {
+		
+		i++;
+		it(`${i}: hex.convertToString - Should convert hex to string`, function() {
 			let expected = "test";
 			let actual = helpful.hex.convertToString("74657374");
 			assert.equal(expected, actual);
 		});
-		it("3: Should convert bytes to hex", function() {
+		
+		i++;
+		it(`${i}: hex.convertFromBytes - Should convert bytes to hex`, function() {
 			let expected = "74657374";
 			let actual = helpful.hex.convertFromBytes(new Uint8Array([116, 101, 115, 116]));
 			assert.equal(expected, actual);
 		});
-		it("4: Should convert hex to bytes", function() {
+		
+		i++;
+		it(`${i}: hex.convertToBytes - Should convert hex to bytes`, function() {
 			let expected = new Uint8Array([116, 101, 115, 116]);
 			let actual = helpful.hex.convertToBytes("74657374");
 			assert.deepEqual(expected, actual);
