c83e814597c7548c887beb9f75152d8a855e55bd
Kabir R <contact@kabirwearsato.ga>
2021-12-01 08:49:24 -0800

    Fix browser support

--- helpful.js
+++ helpful.js
@@ -4,7 +4,7 @@
 	} else if (typeof module === 'object' && module.exports) {
 		module.exports = factory();
 	} else {
-		root.returnExports = factory();
+		root.helpful = factory();
 	}
 }(typeof self !== 'undefined' ? self : this, function() {
 	const helpful = {};
